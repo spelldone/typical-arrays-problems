14e8051d298a8d8eb22a4482f5de1fbb4c72aeac
spelldone <spelldone1@gmail.com>
2021-03-09 23:59:25 +0400

    feat: add implementation of min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if(min.arguments.length==0||array.length==0)
   return 0;
+  let result=0
+  let c=0
+  for(i=0;i<array.length;i++){
+    result=array[i]
+    if(result<c)
+    c=result
+  }
+  return c;
 }
 
 exports.max = function max (array) {
+  if(max.arguments.length==0||array.length==0)
   return 0;
+  let result
+  let c=0
+  for(k=0;k<array.length;k++){
+    result=array[k]
+    if(result>c)
+    c=result
+  }
+  return c;
 }
 
 exports.avg = function avg (array) {
+  if(avg.arguments.length==0||array.length==0)
   return 0;
+  let sum=0;
+  for(i=0;i<array.length;i++){
+    sum+=array[i]
+  }
+
+  return sum/array.length;
 }
